fd409633888f069e7af483cef3afee14265d3123
Burenkovuser <nb2207@yandex.ru>
2019-09-03 21:02:08 +0700

    save and fetch Data

--- MyCoreDataDemo/ViewController.swift
+++ MyCoreDataDemo/ViewController.swift
@@ -7,14 +7,145 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    private let cellID = "cell"
+    //private var tasks: [String] = []
+    private var tasks: [Task] = []
+    //Свойство appdelegate является экзаплятром класса appdelegate и через него будем иметь доспуп к persistentContainer
+    //private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        
+        // Table view cell register
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+    // вызываем метод восстановления в этом методе, потому что каждый раз возвращаясь в это окно он будет взывается в отличие отviewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchData()
+    }
+    
+    // Setup view
+    private func setupView() {
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    // Setup navigation title
+    private func setupNavigationBar() {
+        
+        // Set title for navigation bar
+        title = "Tasks list"
+        
+        // Title color
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        // Navigation bar color
+        navigationController?.navigationBar.barTintColor = UIColor(
+            displayP3Red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        // Set large title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Add button to navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Add",
+            style: .plain,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        showAlert(title: "New message", message: "What do you whant to do?")
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else {
+                print("The text field is empty")
+                return
+            }
+            
+            // Add new task to tasks array
+            self.save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        
+        //Manage object contecst
+        //let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Entity name
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: managedContext) else { return }
+        
+        // Model instance
+        let task = NSManagedObject(entity: entityDescription, insertInto: managedContext) as! Task
+        
+        task.name = taskName
+        
+        do {
+            try managedContext.save()
+            tasks.append(task)
+            self.tableView.insertRows(
+                at: [IndexPath(row: self.tasks.count - 1, section: 0)],
+                with: .automatic
+            )
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 
-
+    private func fetchData() {
+        
+        // Запрос выборки из базы всех значений по ключу Task
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            tasks = try managedContext.fetch(fetchRequest)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
+
+
+// MARK: - UITableViewDataSource
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        
+        return cell
+    }
+}
+
 
